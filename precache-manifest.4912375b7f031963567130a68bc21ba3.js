e8e4843b8e6ca125afb59449d4c5be1bee13386e
Symbol <Symbolistic@live.com>
2020-05-23 17:36:19 -0400

    Updates

---
+++ precache-manifest.4912375b7f031963567130a68bc21ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decf691e88cf9bc449458061ca3b484d",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "edabff566036a616584e",
+    "url": "/pomodoro-clock/static/js/2.85e4f5df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.85e4f5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd82e960b6a71269d77",
+    "url": "/pomodoro-clock/static/js/main.ad969966.chunk.js"
+  },
+  {
+    "revision": "a4dcdb46a55158e82e38",
+    "url": "/pomodoro-clock/static/js/runtime-main.6695a290.js"
+  }
+]);
